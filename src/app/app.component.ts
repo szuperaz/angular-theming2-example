8ba7cc130b528476a83322843545bbc7200b1310
szuperaz <zita.szupera@getstream.io>
2022-07-29 16:07:38 +0200

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,42 @@ import {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  levels = [
+    'No customization',
+    'Theme variables',
+    'Component variables',
+    'Custom CSS code',
+  ];
+  selectedLevelIndex = 0;
   constructor(
     private chatService: ChatClientService,
     private channelService: ChannelService,
     private streamI18nService: StreamI18nService
   ) {
-    const apiKey = '';
-    const userId = '';
-    const userToken = '';
+    const apiKey = 'dz5f4d5kzrue';
+    const userId = 'winter-rice-8';
+    const userToken =
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoid2ludGVyLXJpY2UtOCIsImV4cCI6MTY1OTEyNDU2Nn0.6Lv4GZkQXVxFq-HyPlAXVkoP-AW9veHtcoVcfTsynRM';
     this.chatService.init(apiKey, userId, userToken);
     this.streamI18nService.setTranslation();
   }
 
   async ngOnInit() {
+    const channel = this.chatService.chatClient.channel(
+      'messaging',
+      'talking-about-angular',
+      {
+        // add as many custom fields as you'd like
+        name: 'Talking about Angular',
+      }
+    );
+    await channel.create();
     this.channelService.init({
       type: 'messaging',
-      members: { $in: ['little-wood-9'] },
+      id: { $eq: 'talking-about-angular' },
     });
   }
 }
