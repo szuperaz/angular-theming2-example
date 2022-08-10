4db43c08939829461b857506e388a816746ea1c9
szuperaz <zita.szupera@getstream.io>
2022-08-10 16:20:46 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {
   ChannelService,
   StreamI18nService,
 } from 'stream-chat-angular';
+import dockerNames = require('docker-names');
 
 @Component({
   selector: 'app-root',
@@ -23,27 +24,17 @@ export class AppComponent implements OnInit {
     private channelService: ChannelService,
     private streamI18nService: StreamI18nService
   ) {
-    const apiKey = 'dz5f4d5kzrue';
-    const userId = 'winter-rice-8';
-    const userToken =
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoid2ludGVyLXJpY2UtOCIsImV4cCI6MTY1OTEyNDU2Nn0.6Lv4GZkQXVxFq-HyPlAXVkoP-AW9veHtcoVcfTsynRM';
-    this.chatService.init(apiKey, userId, userToken);
     this.streamI18nService.setTranslation();
   }
 
   async ngOnInit() {
-    const channel = this.chatService.chatClient.channel(
-      'messaging',
-      'talking-about-angular',
-      {
-        // add as many custom fields as you'd like
-        name: 'Talking about Angular',
-      }
-    );
-    await channel.create();
-    this.channelService.init({
-      type: 'messaging',
-      id: { $eq: 'talking-about-angular' },
-    });
+    const apiKey = 'dz5f4d5kzrue';
+    const userId = dockerNames.getRandomName();
+    console.log(userId);
+    // await this.chatService.init(apiKey, userId, 'guest');
+    // this.channelService.init({
+    //   type: 'messaging',
+    //   id: { $eq: 'talking-about-angular' },
+    // });
   }
 }
